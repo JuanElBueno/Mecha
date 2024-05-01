6d4c881a19a5fe87775ff56595bc0e5fdb3c083e
JuanElBueno <85785935+JuanElBueno@users.noreply.github.com>
2024-05-01 11:05:18 +0200

    Update Servicios.bat

--- Servicios.bat
+++ Servicios.bat
@@ -40,7 +40,7 @@ goto sinconexioni
 
 
 :CheckForUpdates
-set Version=1.70.1.3
+set Version=1.71
 set Versiontwo=%Version%
 IF NOT EXIST "%ruta%" md "%ruta%"
 if exist "%ruta%\Updater.bat" DEL /S /Q /F "%ruta%\Updater.bat" >nul 2>&1
